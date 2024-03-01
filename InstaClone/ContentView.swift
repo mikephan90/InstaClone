6818bf54789afe642be3195d7a8c49ce77c94cf6
Mike Phan <khoaphan90@gmail.com>
2024-03-01 11:30:31 -0500

    Initial Commit

---
+++ InstaClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstaClone
+//
+//  Created by Mike Phan on 3/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Mike Phan on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
